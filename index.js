cf6b61e6574f788af262762858bd6c746b033256
ssskram <paulmarks211@gmail.com>
2020-03-19 15:05:27 -0600

    moved commit out of first line to prohibit duplicate comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const axios = require("axios");
-const { stripIndents } = require("common-tags");
-const { Toolkit } = require("actions-toolkit");
+const axios = require('axios');
+const { stripIndents } = require('common-tags');
+const { Toolkit } = require('actions-toolkit');
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
   const actionConfig = {
     zeitToken: process.env.ZEIT_TOKEN,
     teamId: process.env.ZEIT_TEAMID,
-    projectName: process.env.PROJECT_NAME
+    projectName: process.env.PROJECT_NAME,
   };
 
   if (!actionConfig.zeitToken) {
@@ -15,8 +15,8 @@ Toolkit.run(async tools => {
   }
 
   const zeitAPIClient = axios.create({
-    baseURL: "https://api.zeit.co",
-    headers: { Authorization: `Bearer ${actionConfig.zeitToken}` }
+    baseURL: 'https://api.zeit.co',
+    headers: { Authorization: `Bearer ${actionConfig.zeitToken}` },
   });
 
   function fetchLastDeployment() {
@@ -33,10 +33,10 @@ Toolkit.run(async tools => {
 
   const { data: comments } = await tools.github.issues.listComments({
     ...tools.context.repo,
-    issue_number: tools.context.payload.pull_request.number
+    issue_number: tools.context.payload.pull_request.number,
   });
 
-  const commentFirstSentence = `**${deployment.name}** ready! (commit ${deployment.meta.commit})`;
+  const commentFirstSentence = `**${deployment.name}** ready!`;
   const zeitPreviewURLComment = comments.find(comment =>
     comment.body.startsWith(commentFirstSentence)
   );
@@ -44,6 +44,8 @@ Toolkit.run(async tools => {
   const commentBody = stripIndents`
     ${commentFirstSentence}
 
+    commit ${deployment.meta.commit}
+    
     https://${deployment.url}
   `;
 
@@ -51,13 +53,13 @@ Toolkit.run(async tools => {
     await tools.github.issues.updateComment({
       ...tools.context.repo,
       comment_id: zeitPreviewURLComment.id,
-      body: commentBody
+      body: commentBody,
     });
   } else {
     await tools.github.issues.createComment({
       ...tools.context.repo,
       issue_number: tools.context.payload.pull_request.number,
-      body: commentBody
+      body: commentBody,
     });
   }
 });
